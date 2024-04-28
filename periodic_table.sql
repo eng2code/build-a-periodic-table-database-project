6d1854ec07ea34b7c05e7f4c8aed2e6845033cc8
eng2code <amelia.karim@gmail.com>
2024-04-28 19:21:28 +0300

    Update periodic_table.sql

--- periodic_table.sql
+++ periodic_table.sql
@@ -66,7 +66,7 @@ CREATE TABLE public.properties (
     atomic_mass numeric NOT NULL,
     melting_point_celsius numeric NOT NULL,
     boiling_point_celsius numeric NOT NULL,
-    type_id integer NOT NULL
+    type_id integer DEFAULT 1 NOT NULL
 );
 
 
@@ -97,6 +97,8 @@ INSERT INTO public.elements VALUES (6, 'C', 'Carbon');
 INSERT INTO public.elements VALUES (7, 'N', 'Nitrogen');
 INSERT INTO public.elements VALUES (8, 'O', 'Oxygen');
 INSERT INTO public.elements VALUES (1000, 'MT', 'moTanium');
+INSERT INTO public.elements VALUES (9, 'F', 'Fluorine');
+INSERT INTO public.elements VALUES (10, 'Ne', 'Neon');
 
 
 --
@@ -112,6 +114,8 @@ INSERT INTO public.properties VALUES (6, 'nonmetal', 12.011, 3550, 4027, 1);
 INSERT INTO public.properties VALUES (7, 'nonmetal', 14.007, -210.1, -195.8, 1);
 INSERT INTO public.properties VALUES (8, 'nonmetal', 15.999, -218, -183, 1);
 INSERT INTO public.properties VALUES (1000, 'metalloid', 1, 10, 100, 3);
+INSERT INTO public.properties VALUES (9, 'nonmetal', 18.998, -220, -188.1, 1);
+INSERT INTO public.properties VALUES (10, 'nonmetal', 20.18, -248.6, -246.1, 1);
 
 
 --
